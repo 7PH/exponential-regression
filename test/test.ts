f3444c46bd00cf02d249ceeef86e68123af47b8e
Benjamin Raymond <b.raymond@protonmail.com>
2018-07-19 13:50:28 +0200

    improve test - add plot titles for powerspy

--- test/test.ts
+++ test/test.ts
@@ -4,11 +4,53 @@ import * as fs from "fs";
 
 // store path, can be a currently opened file in PowerSpy
 const STORE_PATH: string = '/afs/cern.ch/user/b/braymond/Downloads/files/expreg.csv';
-const N: number = 50;
+const N: number = 100;
+let yOffset: number = 0;
+const yRange: number = - 25;
 const configs: TestConfig[] = [];
 configs.push({
+    title: 'diverging, range ok',
+    a: (yOffset = yOffset + yRange),
+    b: 10,
+    c: 0.02,
+    XMIN: 0,
+    XMAX: 100,
+    N: N,
+    RANDOM: 3
+});
+configs.push({
+    title: 'diverging, almost no noise',
+    a: (yOffset = yOffset + yRange),
+    b: 10,
+    c: 0.02,
+    XMIN: 0,
+    XMAX: 100,
+    N: N,
+    RANDOM: 0.1
+});
+configs.push({
+    title: 'diverging, bad range',
+    a: (yOffset = yOffset + yRange),
+    b: 10,
+    c: 0.01,
+    XMIN: 40,
+    XMAX: 60,
+    N: N,
+    RANDOM: 3
+});
+configs.push({
+    title: 'diverging, almost no noise, little range',
+    a: (yOffset = yOffset + yRange),
+    b: 10,
+    c: 0.01,
+    XMIN: 40,
+    XMAX: 60,
+    N: N,
+    RANDOM: 0.1
+});
+configs.push({
     title: 'converging quick, range ok',
-    a: - 10,
+    a: (yOffset = yOffset + yRange),
     b: - 10,
     c: - 0.2,
     XMIN: 0,
@@ -17,8 +59,18 @@ configs.push({
     RANDOM: 3
 });
 configs.push({
+    title: 'converging real quick, range ok',
+    a: (yOffset = yOffset + yRange),
+    b: - 10,
+    c: - 2,
+    XMIN: 0,
+    XMAX: 20,
+    N: N,
+    RANDOM: 3
+});
+configs.push({
     title: 'converging quick, range too wide',
-    a: 0,
+    a: (yOffset = yOffset + yRange),
     b: - 10,
     c: - 0.2,
     XMIN: 0,
@@ -27,8 +79,8 @@ configs.push({
     RANDOM: 3
 });
 configs.push({
-    title: 'converging',
-    a: 10,
+    title: 'converging, almost no noise',
+    a: (yOffset = yOffset + yRange),
     b: 10,
     c: - 0.05,
     XMIN: 0,
@@ -37,34 +89,14 @@ configs.push({
     RANDOM: 0.1
 });
 configs.push({
-    title: 'converging',
-    a: 20,
+    title: 'converging, almost no noise',
+    a: (yOffset = yOffset + yRange),
     b: 10,
     c: - 0.5,
     XMIN: 0,
     XMAX: 100,
     N: N,
     RANDOM: 0.1
-});
-configs.push({
-    title: 'diverging',
-    a: 30,
-    b: 10,
-    c: 0.01,
-    XMIN: 0,
-    XMAX: 100,
-    N: N,
-    RANDOM: 3
-});
-configs.push({
-    title: 'diverging',
-    a: 40,
-    b: 10,
-    c: 0.01,
-    XMIN: 40,
-    XMAX: 60,
-    N: N,
-    RANDOM: 3
 });
 
 
@@ -104,15 +136,18 @@ const plots: any[] = [];
 
 for (let i = 0; i < configs.length; ++ i) {
     const config: TestConfig = configs[i];
+    let title: string = config.title
+        .replace(/[, ]+/g, '-')
+        .replace(/[-]+/g, '-');
 
     let f0: (x: number) => number = ExpTools.getExp(config.a, config.b, config.c);
     let f1: (x: number) => number = ExpTools.getRandomizedExp(config.a, config.b, config.c, config.RANDOM);
 
     // get points
     let pts0: { xk: number[], yk: number[] } = ExpTools.getPoints(f0, config.XMIN, config.XMAX, config.N);
-    const plot0: { xk: number[], yk: number[], title: string } = {xk: pts0.xk, yk: pts0.yk, title: 'REAL0'};
+    const plot0: { xk: number[], yk: number[], title: string } = {xk: pts0.xk, yk: pts0.yk, title: title + '-real-0'};
     let pts1: { xk: number[], yk: number[] } = ExpTools.getPoints(f1, config.XMIN, config.XMAX, config.N);
-    const plot1: { xk: number[], yk: number[], title: string } = {xk: pts1.xk, yk: pts1.yk, title: 'REAL1'};
+    const plot1: { xk: number[], yk: number[], title: string } = {xk: pts1.xk, yk: pts1.yk, title: title + '-real-1'};
 
     // result
     let solvedVariables: { a: number, b: number, c: number } = ExpReg.solve(pts1.xk, pts1.yk);
@@ -120,7 +155,7 @@ for (let i = 0; i < configs.length; ++ i) {
     // get points
     let solved: (x: number) => number = ExpTools.getExp(solvedVariables.a, solvedVariables.b, solvedVariables.c);
     let pts2: { xk: number[], yk: number[] } = ExpTools.getPoints(solved, config.XMIN, config.XMAX, config.N);
-    const plot2: { xk: number[], yk: number[], title: string } = {xk: pts2.xk, yk: pts2.yk, title: 'APPROX'};
+    const plot2: { xk: number[], yk: number[], title: string } = {xk: pts2.xk, yk: pts2.yk, title: title + '-approx'};
 
     plots.push(plot0, plot1, plot2);
 
